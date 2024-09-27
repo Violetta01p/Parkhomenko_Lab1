6e414c7a97310435ec9bc40077954221fe950108
Violetta01p <pvsergeevnaa@gmail.com>
2024-09-27 10:30:30 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,1 +1,22 @@
-код
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    double root = sqrt(num);
+    
+    if (root == int(root)) {
+
+        cout << num << " is the square of " << int(root) << endl;
+    } else {
+        cout << num << " is not the square of any number" << endl;
+    }
+
+    return 0;
+}
+
